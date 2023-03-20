b22c0f4ce02cc75bbd9da10f1ae2fa2ca95231d6
VladSP <sp.vlad96@gmail.com>
2023-03-20 23:05:46 +0200

    second commit

--- markup/markup/js/main.js
+++ markup/markup/js/main.js
@@ -1,29 +1,32 @@
-var suit = ['clubs', 'spades', 'diamonds', 'hearts'];
-var picture = ['jack', 'queen', 'king', 'ace'];
-var cards = [];
+let suit =['clubs','spades','diamonds','hearts'];
+let picture = ['jack','queen','king','t'];
+let cards = [];
 
-for (var i = 2; i <= 10; i++) {
-  for (var j = 0; j < suit.length; j++) {
-    var cardInfo = `<div class="card__info">${i}<img src="images/${suit[j]}.svg" alt="${suit[j]}"></div>`;
+for(let i=2; i <= 10; i++){
+    for(let j=0; j < suit.length; j++){
+        cardInfo=`<div class="card__info">${i}<img src="images/${suit[j]}.svg" alt="${suit[j]}"></div>`;
 
     cards.push(`<div class="card">
-            ${cardInfo}
-            ${cardInfo}
-        </div>`)
-  }
+        ${cardInfo}
+        ${cardInfo}
+    </div>`);
+    }
 }
 
-for (var k = 0; k < picture.length; k++) {
-  for (var l = 0; l < suit.length; l++) {
-  var pictureSymbol = (['jack', 'queen', 'king',].includes(picture[k])) ? picture[k][0].toUpperCase() : (picture[k] === 'ace') ? 'T' : picture[k];
-    var cardInfo = `<div class="card__info">${pictureSymbol}<img src="images/${suit[l]}.svg" alt="${suit[l]}"></div>`;
-    cards.push(`<div class="card card--person">
-            ${cardInfo}
-            <img class="person" src="images/${picture[k]}.svg" alt="${picture[k]}">
-            ${cardInfo}
-        </div>`)
-  }
-}      
+for(let i = 0; i < picture.length; i++){
+    for(let j = 0; j < suit.length; j++){
+        cardInfo=`<div class="card__info">
+            ${picture[i][0].toUpperCase()}
+            <img src="images/${suit[j]}.svg" alt="${suit[j]}">
+        </div>`;
 
-document.write(`<div class="wrapper">${cards.join('')}</div>`);
+        centerImg = picture[i] !== 't' ? picture[i] : suit[j];
 
+        cards.push(`<div class="card card--person">
+            ${cardInfo}
+            <img class="person" src="images/${centerImg}.svg" alt="${centerImg}">
+            ${cardInfo}
+        </div>`);
+    }
+}
+document.write(`<div class=wrapper>${cards.join(``)}</div>`);
